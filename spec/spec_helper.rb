32d75f7179cb80a6e21a1a12f791ab6250f8735d
Roberto Soares <roberto.tech@gmail.com>
2012-11-01 12:15:35 -0300

    added factories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'minitest/autorun'
+require_relative 'support/factories'
 
 module MiniTest
   class Spec
+    include Factories
     class << self
       alias_method :context, :describe
     end
