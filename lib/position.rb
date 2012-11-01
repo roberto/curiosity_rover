3e241a1eef3a780a66f89818f75ed9cb69549964
Roberto Soares <roberto.tech@gmail.com>
2012-11-01 15:52:20 -0300

    position: added == and move_to(direction)

--- lib/position.rb
+++ lib/position.rb
@@ -5,5 +5,17 @@ class Position
     @x, @y = x, y
   end
 
+  def move_to(direction)
+    case direction
+    when :north then @y+=1
+    when :south then @y-=1
+    when :east  then @x+=1
+    when :west  then @x-=1
+    end
+  end
+
+  def ==(other)
+    self.x == other.x && self.y == other.y
+  end
 
 end
--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -17,4 +17,58 @@ describe Position do
       @position.y.must_equal @y
     end
   end
+
+  describe "==" do
+    context "same x and y" do
+      it "must return true" do
+        Position.new(15,15).must_equal Position.new(15,15)
+      end
+    end
+
+    context "diff x" do
+      it "must return false" do
+        Position.new(15,15).wont_equal Position.new(0,15)
+      end
+    end
+
+    context "diff y" do
+      it "must return false" do
+        Position.new(15,15).wont_equal Position.new(15,0)
+      end
+    end
+  end
+
+  describe "move_to(direction)" do
+    before do
+      @position = Position.new(10, 10)
+    end
+
+    context "direction is north" do
+      it "must increment y" do
+        @position.move_to(:north)
+        @position.must_equal Position.new(10,11)
+      end
+    end
+
+    context "direction is south" do
+      it "must decrement y" do
+        @position.move_to(:south)
+        @position.must_equal Position.new(10,9)
+      end
+    end
+
+    context "direction is east" do
+      it "must increment x" do
+        @position.move_to(:east)
+        @position.must_equal Position.new(11,10)
+      end
+    end
+
+    context "direction is west" do
+      it "must decrement x" do
+        @position.move_to(:west)
+        @position.must_equal Position.new(9,10)
+      end
+    end
+  end
 end
