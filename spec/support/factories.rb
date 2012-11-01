812a6db1f11e00291d32cdf4be043390b3c16532
Roberto Soares <roberto.tech@gmail.com>
2012-11-01 14:59:41 -0300

    factory: minor change

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -2,7 +2,7 @@ module Factories
 
   def create_robot(attrs)
     attrs = {position: Position.new(2,3), direction: :north}.merge(attrs)
-    @robot = Robot.new(*attrs.values)
+    Robot.new(*attrs.values)
   end
 
 end
